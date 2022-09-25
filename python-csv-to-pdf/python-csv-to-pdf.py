85f167beddb9691f38e432a1e029d2ed52b63bbb
Hidekithiago <39059176+Hidekithiago@users.noreply.github.com>
2022-09-24 22:18:00 -0300

    up

--- python-csv-to-pdf/python-csv-to-pdf.py
+++ python-csv-to-pdf/python-csv-to-pdf.py
@@ -1,36 +1,5 @@
-import csv
-from fpdf import FPDF
-
-with open('student.csv', newline='') as f:
-	reader = csv.reader(f)
-	
-	pdf = FPDF()
-	pdf.add_page()
-	page_width = pdf.w - 2 * pdf.l_margin
-		
-	pdf.set_font('Times','B',14.0) 
-	pdf.cell(page_width, 0.0, 'Students Data', align='C')
-	pdf.ln(10)
-
-	pdf.set_font('Courier', '', 12)
-	
-	col_width = page_width/4
-	
-	pdf.ln(1)
-	
-	th = pdf.font_size
-	
-	for row in reader:
-        #print(row)
-		pdf.cell(col_width, th, str(row[0]), border=1)
-		pdf.cell(col_width, th, row[1], border=1)
-		pdf.cell(col_width, th, row[2], border=1)
-		pdf.cell(col_width, th, row[3], border=1)
-		pdf.ln(th)
-		
-	pdf.ln(10)
-
-	pdf.set_font('Times','',10.0) 
-	pdf.cell(page_width, 0.0, '- end of report -', align='C')
-	
-	pdf.output('student.pdf', 'F')
+#https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf
+import pdfkit
+path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+pdfkit.from_file(r'C:\Users\hidek\Downloads\Manserv\asdasd.html',r'C:\Users\hidek\Downloads\Manserv\asdasd.pdf', configuration=config)
